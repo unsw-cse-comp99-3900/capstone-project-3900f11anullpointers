21bfbbf584164f90b0ea17e212ef99ed93e21e2a
PepsiNick <nicholas.abreu@outlook.com>
2024-06-24 13:37:33 +1000

    Added font customisation with a json file

--- backend/pdf_gen.py
+++ backend/pdf_gen.py
@@ -1,28 +1,54 @@
 from fpdf import FPDF
+import json
+
+FONT_CONFIG = "font_config.json"
 
 def generate_PDF(token: str, name: str, form_name: str) -> None:
-    pdf = FPDF()
+    pdf: object = FPDF()
     pdf.add_page()
+
+    fonts = get_fonts()
     
+    # Generate title
+    pdf.add_font(fonts["title"], "", "fonts/" + fonts["title"] + ".ttf", uni=True)
+    pdf.set_font(fonts["title"], size=20)
+    pdf.cell(200, 20, txt = get_title(form_name), ln = True, align = 'C')
+
+    # Generate subtitle
+    pdf.add_font(fonts["subtitle"], "", "fonts/" + fonts["subtitle"] + ".ttf", uni=True)
+    pdf.set_font(fonts["subtitle"], size=16)
+    pdf.cell(200, 15, txt = get_subtitle(form_name), ln = True, align = 'L')
+
+    # Generate text
+    pdf.add_font(fonts["body"], "", "fonts/" + fonts["body"] + ".ttf", uni=True)
+    pdf.set_font(fonts["body"], size=12)
+    pdf.multi_cell(0, 5, txt = get_body(form_name))
+
+    pdf.output(token + ".pdf")
+
+def get_fonts():
+    with open(FONT_CONFIG, "r") as file:
+        fonts = json.load(file)
+
+    return fonts
+
+def get_title(form_name: str) -> str:
     with open("form_text/" + form_name + "/title.txt", "r") as file:
         title = file.read()
 
+    return title
+
+def get_subtitle(form_name: str) -> str:
     with open("form_text/" + form_name + "/subtitle.txt", "r") as file:
         subtitle = file.read()
 
-    with open("form_text/" + form_name + "/text.txt", "r") as file:
+    return subtitle
+
+def get_body(form_name: str) -> str:
+    with open("form_text/" + form_name + "/body.txt", "r") as file:
         text = file.read()
-    
-    pdf.set_font("Arial", size=20)
-    pdf.cell(200, 10, txt = title, ln = True, align = 'C')
 
-    pdf.set_font("Arial", size=14)
-    pdf.cell(200, 10, txt = subtitle, ln = True, align = 'L')
-
-    pdf.set_font("Arial", size=12)
-    pdf.multi_cell(0, 5, txt = text)
-
-    pdf.output(token + ".pdf")
+    return text
 
 
 def main():
