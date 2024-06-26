84144427feceb7610c3d769fe18844f58bb71343
dk-wiki <goodlittlebirdie54@gmail.com>
2024-06-26 19:16:26 +1000

    Added footer and simple application consent example - will be changed

--- frontend/src/components/Header.tsx
+++ frontend/src/components/Header.tsx
@@ -7,11 +7,11 @@ const Header = () => {
   const { theme } = useThemeContext();
 
   return (
-    <header className={`bg-background text-foreground border-b border-border w-full ${theme === "dark" ? "dark-mode-class" : "light-mode-class"}`}>
-      <div className="container mx-auto flex items-center justify-between py-4 px-4">
-        <div className="flex items-center space-x-4">
-          <img src="/unsw_logo.png" alt="Logo" className="h-[80px] w-auto" />
-          <h1 className="font-heading text-3xl md:text-4xl text-primary">
+    <header className={`bg-primary-foreground text-foreground border-b border-border w-full ${theme === "dark" ? "dark-mode-class" : "light-mode-class"}`}>
+      <div className="container mx-auto flex items-center justify-between py-2 md:py-4 px-4">
+        <div className="flex items-center space-x-2 md:space-x-4">
+          <img src="/unsw_logo.png" alt="Logo" className="h-10 w-auto md:h-[80px]" />
+          <h1 className="font-heading text-xl md:text-3xl lg:text-4xl text-primary">
             UNSW Optometry Clinic Stuff
           </h1>
         </div>
