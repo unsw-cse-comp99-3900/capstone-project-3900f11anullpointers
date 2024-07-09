be62c4cd586a9e27cb5b8c22cbfa49f1b4111e07
Moaesaycto <stephen.lerantges.los@gmail.com>
2024-07-09 18:07:20 +1000

    Updated mode changing buttons

--- frontend/src/components/ModeToggle.tsx
+++ frontend/src/components/ModeToggle.tsx
@@ -1,6 +1,6 @@
 "use client";
 import * as React from "react";
-import { Moon, Sun, Type } from "lucide-react";
+import { Moon, Sun, ZoomIn, ZoomOut } from "lucide-react";
 import { useThemeContext } from "@/context/theme-context";
 import { Button } from "@/components/ui/button";
 
@@ -9,23 +9,64 @@ export function ModeToggle() {
 
   return (
     <div className="flex space-x-2">
-      <Button variant="outline" size="icon" onClick={toggleTheme}>
+      <Button
+        variant="outline"
+        size={textLarge ? "lg" : "icon"}
+        onClick={toggleTheme}
+      >
         {theme === "dark" ? (
           <>
-            <Sun className="h-[1.2rem] w-[1.2rem] transition-all rotate-90 scale-0" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] transition-all rotate-0 scale-100" />
+            <Sun
+              className={`${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-90 scale-0`}
+            />
+            <Moon
+              className={`absolute ${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-0 scale-100`}
+            />
           </>
         ) : (
           <>
-            <Sun className="h-[1.2rem] w-[1.2rem] transition-all rotate-0 scale-100" />
-            <Moon className="absolute h-[1.2rem] w-[1.2rem] transition-all rotate-90 scale-0" />
+            <Sun
+              className={`${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-0 scale-100`}
+            />
+            <Moon
+              className={`absolute ${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-90 scale-0`}
+            />
           </>
         )}
         <span className="sr-only">Toggle theme</span>
       </Button>
-      <Button variant="outline" size="icon" onClick={toggleTextSize}>
-        <Type className="h-[1.2rem] w-[1.2rem]" />
-        <span className="sr-only">Toggle large text</span>
+      <Button
+        variant="outline"
+        size={textLarge ? "lg" : "icon"}
+        onClick={toggleTextSize}
+      >
+        {textLarge ? (
+          <>
+            <ZoomIn
+              className={`${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-90 scale-0`}
+            />
+            <ZoomOut
+              className={`absolute ${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-0 scale-100`}
+            />
+          </>
+        ) : (
+          <>
+            <ZoomIn
+              className={`${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-0 scale-100`}
+            />
+            <ZoomOut
+              className={`absolute ${textLarge ? "h-6 w-6" : "h-[1.2rem] w-[1.2rem]"
+                } transition-all rotate-90 scale-0`}
+            />
+          </>
+        )}
       </Button>
     </div>
   );
