77596bb627c7ab3c6b4cef0d38b39606a2735f69
Tom Mihaljevic <z5361148@cse.unsw.edu.au>
2024-07-23 15:12:47 +1000

    Send email tests updated for new structure

--- backend/tests/test_send_email.py
+++ backend/tests/test_send_email.py
@@ -1,77 +1,127 @@
 import unittest
 from unittest.mock import patch, MagicMock, ANY
-from io import BytesIO
-import smtplib
-from src.send_email import send_emails 
+import smtplib, base64
+from datetime import datetime
+from src.send_email import send_email_to_clinic,  send_email_to_patient
 
-class TestSendEmails(unittest.TestCase):
-    
+class TestSendClinicEmails(unittest.TestCase):
+    def setUp(self) -> None:
+        self.server = "smtp.gmail.com"
+        self.port = 587
+        self.email_from = "anullpointers@gmail.com"
+        self.email_to = "z5361148@ad.unsw.edu.au"
+        self.pswd = "jrowigmvzvtoifhz"
+        self.attachment_name = "bobs_consent_info.pdf"
+        self.attachment_content = base64.b64encode(b'pdf content').decode('utf-8')
+        self.patient_name = 'Bob Marley'
+        self.patient_email = "z5361148@ad.unsw.edu.au"
+        self.datetime = datetime.now()
+        
     @patch('smtplib.SMTP')
-    def test_send_emails_success(self, mock_smtp):
+    def test_send_clinic_email_success(self, mock_smtp):
         # Mock the SMTP server
         mock_server = MagicMock()
         mock_smtp.return_value = mock_server
 
-        server = "smtp.gmail.com"
-        port = 587
-        email_from = "anullpointers@gmail.com"
-        email_to = "z5361148@ad.unsw.edu.au"
-        pswd = "rtrjqzvtyuecbyxh"
-        attachment_name = "src/test1.pdf"
-        attachment_content = BytesIO(b"PDF content")
-        
         # Call the send_emails function
-        send_emails(server, port, email_from, email_to, pswd, attachment_name, attachment_content)
+        send_email_to_clinic(self.server, self.port, self.email_from, self.email_to, 
+                             self.pswd, self.attachment_name, self.attachment_content,
+                             self.patient_name, self.patient_email, self.datetime)
 
         # Assertions to check if email sending process is called correctly
-        mock_smtp.assert_called_with(server, port)
+        mock_smtp.assert_called_with(self.server, self.port)
         mock_server.starttls.assert_called_once()
-        mock_server.login.assert_called_with(email_from, pswd)
+        mock_server.login.assert_called_with(self.email_from, self.pswd)
         mock_server.sendmail.assert_called_once()
-        mock_server.sendmail.assert_called_with(email_from, email_to, ANY)
-        self.assertIn(email_to, mock_server.sendmail.call_args[0][1])
+        mock_server.sendmail.assert_called_with(self.email_from, self.email_to, ANY)
+        self.assertIn(self.email_to, mock_server.sendmail.call_args[0][1])
 
     @patch('smtplib.SMTP')
-    def test_send_emails_exception(self, mock_smtp):
+    def test_send_clinc_email_exception(self, mock_smtp):
         # Mock the SMTP server to raise an exception
         mock_smtp.side_effect = smtplib.SMTPException("Failed to connect")
-        
-        server = "smtp.example.com"
-        port = 587
-        email_from = "sender@example.com"
-        email_to = "recipient@example.com"
-        pswd = "password"
-        attachment_name = "consent_form.pdf"
-        attachment_content = BytesIO(b"PDF content")
 
         with self.assertRaises(smtplib.SMTPException) as e:
-            send_emails(server, port, email_from, email_to, pswd, attachment_name, attachment_content)
-        self.assertEqual(str(e.exception), "Failed to connect")
+            send_email_to_clinic(self.server, self.port, self.email_from, self.email_to, 
+                                self.pswd, self.attachment_name, self.attachment_content,
+                                self.patient_name, self.patient_email, self.datetime)        
+            self.assertEqual(str(e.exception), "Failed to connect")
         
     @patch('smtplib.SMTP')
-    def test_send_emails_message_content(self, mock_smtp):
+    def test_send_clinic_email_message_content(self, mock_smtp):
         # Mock the SMTP server
         mock_server = MagicMock()
         mock_smtp.return_value = mock_server
-
-        server = "smtp.example.com"
-        port = 587
-        email_from = "sender@example.com"
-        email_to = "recipient@example.com"
-        pswd = "password"
-        attachment_name = "consent_form.pdf"
-        attachment_content = BytesIO(b"PDF content")
-        
+        pdf_content = base64.b64encode(b'test that content is correct\nTest1\nTest2').decode('utf-8')
         # Call the send_emails function
-        send_emails(server, port, email_from, email_to, pswd, attachment_name, attachment_content)
+        send_email_to_clinic(self.server, self.port, self.email_from, self.email_to, 
+                        self.pswd, self.attachment_name, pdf_content,
+                        self.patient_name, self.patient_email, self.datetime)
 
         # Get the email content
         email_content = mock_server.sendmail.call_args[0][2]
         
         # Check if the email content is correctly formed
         self.assertIn("Content-Type: text/html", email_content)
-        self.assertIn("Subject: Patient consent form", email_content)
-        self.assertIn("Dear [Recipient's Name],", email_content)
+        self.assertIn("Subject: Patient Consent Form Submission", email_content)
+        self.assertIn("<strong>Patient Name:</strong> Bob Marley", email_content)
+
+class TestSendPatientEmails(unittest.TestCase):
+    def setUp(self) -> None:
+        self.server = "smtp.gmail.com"
+        self.port = 587
+        self.email_from = "anullpointers@gmail.com"
+        self.email_to = "z5361148@ad.unsw.edu.au"
+        self.pswd = "jrowigmvzvtoifhz"
+        self.patient_name = 'Bob Marley'
+
+        
+    @patch('smtplib.SMTP')
+    def test_send_patient_email_success(self, mock_smtp):
+        # Mock the SMTP server
+        mock_server = MagicMock()
+        mock_smtp.return_value = mock_server
+
+        # Call the send_emails function
+        send_email_to_patient(self.server, self.port, self.email_from, self.email_to, 
+                             self.pswd, self.patient_name)
+
+        # Assertions to check if email sending process is called correctly
+        mock_smtp.assert_called_with(self.server, self.port)
+        mock_server.starttls.assert_called_once()
+        mock_server.login.assert_called_with(self.email_from, self.pswd)
+        mock_server.sendmail.assert_called_once()
+        mock_server.sendmail.assert_called_with(self.email_from, self.email_to, ANY)
+        self.assertIn(self.email_to, mock_server.sendmail.call_args[0][1])
+
+    @patch('smtplib.SMTP')
+    def test_send_patient_email_exception(self, mock_smtp):
+        # Mock the SMTP server to raise an exception
+        mock_smtp.side_effect = smtplib.SMTPException("Failed to connect")
+
+        with self.assertRaises(smtplib.SMTPException) as e:
+            send_email_to_patient(self.server, self.port, self.email_from, self.email_to, 
+                             self.pswd, self.patient_name)      
+            self.assertEqual(str(e.exception), "Failed to connect")
+        
+    @patch('smtplib.SMTP')
+    def test_send_patient_email_message_content(self, mock_smtp):
+        # Mock the SMTP server
+        mock_server = MagicMock()
+        mock_smtp.return_value = mock_server
+        
+        # Call the send_emails function
+        send_email_to_patient(self.server, self.port, self.email_from, self.email_to, 
+                             self.pswd, self.patient_name)
+
+        # Get the email content
+        email_content = mock_server.sendmail.call_args[0][2]
+        
+        # Check if the email content is correctly formed
+        self.assertIn("Content-Type: text/html", email_content)
+        self.assertIn("Subject: Confirmation of Consent Form Submission - UNSW Optometry Clinic", email_content)
+        self.assertIn("Dear Bob Marley,", email_content)    
+    
 
 
 if __name__ == '__main__':
